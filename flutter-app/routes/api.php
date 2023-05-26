20371979c6812aa8b6ac7cdeb84baa0898da55b2
Chau Vo <chauvo25102003@gmail.com>
2023-05-26 12:19:25 +0700

    chau push

--- flutter-app/routes/api.php
+++ flutter-app/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ApiController\AttendanceController;
 use App\Http\Controllers\ApiController\EventController;
 use App\Http\Controllers\ApiController\StudentController;
+use App\Http\Controllers\ApiController\UserController as ApiControllerUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('students', StudentController::class);
 Route::apiResource('events', EventController::class);
 Route::apiResource('attendances', AttendanceController::class);
+Route::apiResource('users', ApiControllerUserController::class);
